745fcba718d05a2067c74f52f104fbf542f56790
Nirmal Mekala <ndmekala@icloud.com>
2023-12-16 11:49:09 -0600

    refine html, tweak colors, add fonts, add title

--- src/index.js
+++ src/index.js
@@ -13,13 +13,13 @@ const yLabels = data[0].slice(1);
 
 let datasets = [];
 const colors = [
+  '#af005f',
   '#ff5faf',
   '#af87d7',
   '#5fafd7',
   '#00afaf',
   '#5f8787',
   '#5faf5f',
-  '#afd700',
   '#d7875f',
 ];
 
@@ -38,79 +38,110 @@ for (let i = 1; i < data[0].length; i++) {
 }
 
 app.get('/', (req, res) => {
+  res.header('Content-Type', 'text/html');
   res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>CSV Plot</title>
+        <style>
 
-    <style>
-
-    body {
-      background-color: #1c1c1c;
-    }
-
-    .container {
-      display: flex;
-      justify-content: center;
-      padding-top: 50px;
-    }
-
-    .wrapper {
-      width: 100%;
-      max-width: 800px;
-      padding-right: 10px;
-      padding-left: 10px;
-    }
-
-    .chart {
-      background-color: #1c1c1c;
-    }
-
-    </style>
-
-    <body>
-    <div class="container">
-      <div class="wrapper">
-        <canvas class="chart" id="plot"></canvas>
-      </div>
-    </div>
-    <body>
-    
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    
-    <script>
-      const ctx = document.getElementById('plot');
-
-      const grey = '#585858'
-
-      new Chart(ctx, {
-        type: 'scatter',
-        data: ${JSON.stringify({ datasets })},
-        options: {
-          responsive: true,
-          color: grey,
-          showLine: true,
-          scales: {
-            x: {
-              grid: {
-                color: grey,
-              },
-              type: 'linear',
-              position: 'bottom',
-              ticks: {
-                color: grey,
-              }
-            },
-            y: {
-              grid: {
-                color: grey,
-              },
-              ticks: {
-                color: grey,
-              }
-            },
-          }
+        body {
+          background-color: #1c1c1c;
         }
-      });
-    </script>
 
+        h1 {
+          font-family: "FiraCode Nerd Font", monospace;
+          text-align: center;
+          color: #ff5faf;
+          font-weight: normal;
+          font-size: 3rem;
+          margin: 0;
+        }
+
+        .container {
+          display: flex;
+          justify-content: center;
+          padding-top: 50px;
+        }
+
+        .wrapper {
+          width: 100%;
+          max-width: 800px;
+          padding-right: 10px;
+          padding-left: 10px;
+        }
+
+        .chart {
+          background-color: #1c1c1c;
+        }
+
+        </style>
+      </head>
+
+      <body>
+        <div class="container">
+          <div class="wrapper">
+            <h1>CSV Plot</h1>
+            <canvas class="chart" id="plot"></canvas>
+          </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+        <script>
+          const ctx = document.getElementById('plot');
+
+          const grey = '#585858'
+
+          new Chart(ctx, {
+            type: 'scatter',
+            data: ${JSON.stringify({ datasets })},
+            options: {
+              responsive: true,
+              color: grey,
+              showLine: true,
+              plugins: {
+                legend: {
+                  labels: {
+                    font: {
+                      family: 'FiraCode Nerd Font, monospace',
+                    }
+                  },
+                  position: 'bottom',
+                },
+              },
+              scales: {
+                x: {
+                  grid: {
+                    color: grey,
+                  },
+                  type: 'linear',
+                  position: 'bottom',
+                  ticks: {
+                    color: grey,
+                    font: {
+                      family: 'FiraCode Nerd Font, monospace',
+                    }
+                  }
+                },
+                y: {
+                  grid: {
+                    color: grey,
+                  },
+                  ticks: {
+                    color: grey,
+                    font: {
+                      family: 'FiraCode Nerd Font, monospace',
+                    }
+                  }
+                },
+              }
+            }
+          });
+        </script>
+      </body>
+    </html>
     `);
 });
 
