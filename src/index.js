e5113a3b6a08e71d3be3e59a7174f7474892e455
Nirmal Mekala <ndmekala@icloud.com>
2025-04-28 09:10:58 -0500

    stacked charts support

--- src/index.js
+++ src/index.js
@@ -5,42 +5,66 @@ const { generateChartConfig, processXData, readInput } = require('./utils');
 const markup = require('./index.html');
 const { colors, fontFamily } = require('./constants');
 
-const app = express();
-let data = [];
-let xDataType;
-try {
-  let csvPath = readInput(process.argv, 3);
-  data = parse(fs.readFileSync(csvPath, 'utf8'));
-  xDataType = processXData(data);
-} catch (error) {
-  console.error(error.message);
-  process.exit(1);
-}
+main();
 
-const config = generateChartConfig(data, colors, fontFamily, xDataType);
+function main () {
+  const app = express();
+  let data = [];
+  let xDataType;
+  let params = {};
 
-app.get('/api/config', (req, res) => {
-  res.json(config);
-});
-app.get('/', (req, res) => {
-  res.header('Content-Type', 'text/html');
-  res.send(markup);
-});
+  const flags = {
+    stacked: ['--stacked', '-s'],
+  };
 
-const port = 4143;
+  // TODO usage and help flow
+  try {
+    let csvPath = readInput(process.argv);
 
-app.listen(port, () => {
-  console.log(
-    '\x1b[32m%s\x1b[0m',
-    `
-┌─────────────────────────────┐
-│                             │
-│    Chart 📈 on port ${port}    │
-│                             │
-└─────────────────────────────┘
-`,
+    if (flags.stacked.some((flag) => process.argv.includes(flag))) {
+      params.stacked = true;
+    }
+
+    data = parse(fs.readFileSync(csvPath, 'utf8'));
+    xDataType = processXData(data);
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
+  }
+
+  const config = generateChartConfig(
+    data,
+    colors,
+    fontFamily,
+    xDataType,
+    params,
   );
-  import('open').then((module) => {
-    module.default(`http://localhost:${port}`);
+
+  app.get('/api/config', (req, res) => {
+    res.json(config);
   });
-});
+  app.get('/', (req, res) => {
+    res.header('Content-Type', 'text/html');
+    res.send(markup);
+  });
+
+  const port = 4143;
+
+  app.listen(port, () => {
+    console.log(
+      '\x1b[32m%s\x1b[0m',
+      `
+  ┌─────────────────────────────┐
+  │                             │
+  │    Chart 📈 on port ${port}    │
+  │                             │
+  └─────────────────────────────┘
+  `,
+    );
+    import('open').then((module) => {
+      module.default(`http://localhost:${port}`);
+    });
+  });
+};
+
+
